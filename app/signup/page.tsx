cfe9a7a4b187936304c7ff5d8dacaee7b5cff726
jihye1116 <jihyesim170@gmail.com>
2024-11-16 17:28:43 +0900

    FEAT :: 회원 가입 구현

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -3,25 +3,33 @@ import React, { useState, useCallback } from "react";
 import InputField from "../component/common/InputField";
 import Button from "../component/common/Button";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 interface FormData {
-  name: string | undefined;
+  name: string;
   username: string;
   password: string;
   confirmPassword: string;
   email: string;
-  keepLoggedIn: boolean;
-  [key: string]: any;
+  nickname: string;
+  gender: "M" | "W";
+  role: "ROLE_USER" | "ROLE_COMPANY";
+  phone: string;
 }
 
 const SignupPage = () => {
+  const router = useRouter();
+
   const [formData, setFormData] = useState<FormData>({
     name: "",
     username: "",
     password: "",
     confirmPassword: "",
     email: "",
-    keepLoggedIn: false,
+    nickname: "",
+    gender: "M",
+    role: "ROLE_USER",
+    phone: "",
   });
 
   const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
@@ -32,8 +40,43 @@ const SignupPage = () => {
     }));
   }, []);
 
-  const handleSignUpClick = useCallback(() => {
-    console.log(formData);
+  const handleSignUpClick = useCallback(async () => {
+    if (formData.password !== formData.confirmPassword) {
+      alert("비밀번호와 비밀번호 확인이 일치하지 않습니다.");
+      return;
+    }
+
+    const requestBody = {
+      email: formData.email,
+      password: formData.password,
+      nickname: formData.nickname,
+      gender: formData.gender,
+      role: formData.role,
+      phone: formData.phone,
+    };
+
+    try {
+      const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+      const response = await fetch(`${apiUrl}/auth/sign-up`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(requestBody),
+      });
+
+      if (!response.ok) {
+        const errorData = await response.json();
+        alert(`회원가입 실패: ${errorData.message}`);
+      } else {
+        const data = await response.json();
+        alert("회원가입 성공!");
+        router.push("/login");
+      }
+    } catch (error) {
+      alert("회원가입 중 오류가 발생했습니다.");
+      console.error(error);
+    }
   }, [formData]);
 
   const fields = [
@@ -48,14 +91,14 @@ const SignupPage = () => {
       name: "username",
       type: "text",
       isRequired: true,
-      helperText: "아이디 조건",
+      // helperText: "아이디 조건",
     },
     {
       label: "비밀번호",
       name: "password",
       type: "password",
       isRequired: true,
-      helperText: "비밀번호 조건",
+      helperText: "",
     },
     {
       label: "비밀번호 확인",
@@ -69,6 +112,25 @@ const SignupPage = () => {
       type: "email",
       isRequired: true,
     },
+    {
+      label: "닉네임",
+      name: "nickname",
+      type: "text",
+      isRequired: true,
+    },
+    {
+      label: "성별",
+      name: "gender",
+      type: "radio",
+      isRequired: true,
+      options: ["M", "W"],
+    },
+    {
+      label: "전화번호",
+      name: "phone",
+      type: "text",
+      isRequired: true,
+    },
   ];
 
   return (
@@ -77,18 +139,41 @@ const SignupPage = () => {
         회원가입
       </h1>
       <div className="flex flex-col justify-center items-center mt-5 max-w-full">
-        {fields.map((field) => (
-          <InputField
-            key={field.name}
-            label={field.label}
-            name={field.name}
-            type={field.type}
-            isRequired={field.isRequired}
-            value={formData[field.name as keyof FormData] || "ㅇㅇ"}
-            onChange={handleChange}
-            helperText={field.helperText}
-          />
-        ))}
+        {fields.map((field) =>
+          field.type === "radio" ? (
+            <div key={field.name} className="flex flex-col mb-6 w-full">
+              <label className="mb-2 text-black">{field.label}</label>
+              <div className="flex gap-5 ">
+                {field.options?.map((option) => (
+                  <label key={option} className="flex items-center">
+                    <input
+                      type="radio"
+                      name={field.name}
+                      value={option}
+                      checked={formData.gender === option}
+                      onChange={handleChange}
+                      className="mr-2 appearance-none border-2 border-black rounded-full w-4 h-4 checked:bg-black checked:border-black"
+                    />
+                    <span className="text-sm capitalize text-black">
+                      {option === "M" ? "남자" : "여자"}
+                    </span>
+                  </label>
+                ))}
+              </div>
+            </div>
+          ) : (
+            <InputField
+              key={field.name}
+              label={field.label}
+              name={field.name}
+              type={field.type}
+              isRequired={field.isRequired}
+              value={formData[field.name as keyof FormData] || ""}
+              onChange={handleChange}
+              helperText={field.helperText}
+            />
+          )
+        )}
         <div className="flex flex-col mt-5 max-w-full font-bold text-slate-950 w-[400px]">
           <Button onClick={handleSignUpClick} variant="primary">
             회원가입
